1e64cf07a032bdb59e1959fa179eb600ad12f445
ankit <ankit@DESKTOP-274B54E>
2022-11-22 16:24:41 +0530

    Add project files.

---
+++ Clone(MVC)/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.Mvc;
+using Clone_MVC_.Controllers;
+using Clone_MVC_.Models;
+using System.Data.SqlClient;
+namespace Clone_MVC_.Controllers
+{
+    public class AccountController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+
+        // GET: Account
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        void connectionString()
+        {
+            con.ConnectionString= "data source =DESKTOP-274B54E; database=SompoDB; integrated security =SSPI; ";
+
+		}
+        [HttpPost]
+        public ActionResult Verify(Account acc) 
+        {
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "Select * from ASPNETUSER where username='" + acc.Name + "' and password='" + acc.Password + "'";
+			dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                con.Close();
+				return View("Create");
+			}
+            else
+            {
+                con.Close();
+				return View("Error");
+			}
+            
+
+        }
+    }
+}
---
+++ Clone(MVC)/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Clone_MVC_.MvcApplication" Language="C#" %>
